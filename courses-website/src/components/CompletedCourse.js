a5d784c2a6f5f9eba91cde1288979c567a120bd6
dhuhaahmad <dhuha9712@gmail.com>
2021-01-13 23:16:09 +0300

    Fixed the navigation to playCourse component from mylearning and completeed course

--- courses-website/src/components/CompletedCourse.js
+++ courses-website/src/components/CompletedCourse.js
@@ -1,26 +1,64 @@
-import React, { Component } from 'react'
-import Course from './Course'
-import DeleteAllList from './DeleteAllList'
+import React, { Component } from "react";
+import Course from "./Course";
+import DeleteAllList from "./DeleteAllList";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PlayCourse from "./PlayCourse";
 
 export default class CompletedCourse extends Component {
-    render() {
-        
-        const completeCourses = this.props.complete.map((ele,i)=>{
-            return <Course url={ele.snippet.thumbnails.standard.url} 
-            courseTitle={ele.snippet.title}
-             channelName={ele.snippet.channelTitle}
-             playListId={ele.id}
-             handleAddToComplete={()=>this.props.handleAddToComplete(ele)}
-            addToMyLearning={(e)=>this.props.addToMyLearning(ele)}
-            isComplete={true}
-             key={i}
-             />
-            })
-        return (
-            <div>
-                <DeleteAllList removeAll={this.props.removeAllcomplete} />
-                {completeCourses.length >0 ? completeCourses: <h1>No completed course </h1>}
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      toggleRoute: false,
+      playListId: "",
+    };
+  }
+
+  handleToggle = (id) => {
+    this.setState({ toggleRoute: !this.state.toggleRoute, playListId: id });
+  };
+  render() {
+    const completeCourses = this.props.complete.map((ele, i) => {
+      return (
+        <Course
+          url={ele.snippet.thumbnails.standard.url}
+          courseTitle={ele.snippet.title}
+          channelName={ele.snippet.channelTitle}
+          playListId={ele.id}
+          handleAddToComplete={() => this.props.handleAddToComplete(ele)}
+          addToMyLearning={(e) => this.props.addToMyLearning(ele)}
+          isComplete={true}
+          handleToggle={this.handleToggle}
+          key={i}
+        />
+      );
+    });
+    return (
+        <Router>
+      <div>
+        <Link to="/play-course"></Link>
+
+        {this.state.toggleRoute ? (
+          <Route
+            path="/play-course"
+            render={(props) => (
+              <PlayCourse {...props} playListId={this.state.playListId} />
+            )}
+          />
+        ) : (
+          <div>
+            <DeleteAllList removeAll={this.props.removeAllcomplete} />
+            {completeCourses.length > 0 ? (
+              completeCourses
+            ) : (
+              <h1>No completed course </h1>
+            )}
+          </div>
+        )}
+        {/* <DeleteAllList removeAll={this.props.removeAllcomplete} />
+                {completeCourses.length >0 ? completeCourses: <h1>No completed course </h1>} */}
+      </div>
+      </Router>
+    );
+  }
 }
--- courses-website/src/components/MyLearning.js
+++ courses-website/src/components/MyLearning.js
@@ -2,8 +2,31 @@ import React, { Component } from 'react'
 // import CoursesContainer from './CoursesContainer'
 import Course from './Course'
 import DeleteAllList from './DeleteAllList'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PlayCourse from "./PlayCourse";
 
 export default class MyLearning extends Component {
+
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             toggleRoute:false,
+             playListId:''
+             
+        }
+    }
+    
+    handleToggle=(id)=>{
+    
+        this.setState({toggleRoute:!this.state.toggleRoute,
+            playListId:id
+        })
+    
+    }
+
+
     render() {
         
 
@@ -14,17 +37,31 @@ export default class MyLearning extends Component {
              playListId={ele.id}
              addToMyLearning={()=>this.props.addToMyLearning(ele)}
              handleAddToComplete={()=>this.props.handleAddToComplete(ele)}
-
-            //  handlelearning={this.handlelearning}
              islearning={true}
+             handleToggle={this.handleToggle}
              key={i}
              />
             })
+
         return (
+            <Router>
             <div>
-                <DeleteAllList removeAll={this.props.removeAllMyLearning} />
-                {learningCourses.length >0 ? learningCourses: <h1>No course Added</h1>}
+            <Link to="/play-course"></Link>
+
+                {this.state.toggleRoute ?  <Route
+          path="/play-course"
+          render={(props) => (
+            <PlayCourse {...props} playListId={this.state.playListId} />
+          )}
+        /> : (<div>
+            <DeleteAllList removeAll={this.props.removeAllMyLearning} />
+            {learningCourses.length >0 ? learningCourses: <h1>No course Added</h1>}
+        </div>)}
+
+                {/* {learningCourses.length >0 ? learningCourses: <h1>No course Added</h1>} */}
             </div>
+             </Router>
         )
     }
 }
+
